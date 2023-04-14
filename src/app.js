82e485ae1619ea509ecafe69dccbcd54180549bc
Rous Epistola <rous.epistola@accenture.com>
2023-04-14 14:50:06 -0400

    module 2

--- src/app.js
+++ src/app.js
@@ -8,8 +8,28 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
 
+const accountData = fs.readFileSync('src/json/accounts.json', 'utf8')
+const accounts = JSON.parse(accountData)
+
+const userData = fs.readFileSync('src/json/users.json', 'utf8')
+const users = JSON.parse(userData)
+
 app.get('/', (req, res) =>{
-    res.render('index', {title: 'Index'})
+    res.render('index', {title: 'Account Summary', accounts})
+})
+
+app.get('/savings', (req, res)=>{
+    res.render('account',{account:accounts.savings})
+})
+app.get('/checking', (req, res)=>{
+    res.render('account',{account:accounts.savings})
+})
+app.get('/credit', (req, res)=>{
+    res.render('account',{account:accounts.savings})
+})
+
+app.get('/profile',(req, res)=>{
+    res.render('profile', {user:users[0]})
 })
 
 app.listen(3000,()=>{
