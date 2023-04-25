9d1e8b1dc6494f62c4332401e7cdb4fe3fb04846
Rous Epistola <rous.epistola@accenture.com>
2023-04-25 14:24:16 -0400

    fixes

--- src/app.js
+++ src/app.js
@@ -1,90 +1,46 @@
-const fs = require("fs");
-const path = require("path");
-const express = require("express")
+const fs = require('fs');
+const path = require('path');
 
-const app = express()
+const express = require('express');
+const app = new express();
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set('views',path.join(__dirname, '/views'));
+app.set('view engine','ejs');
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: true }));
 
-const accountData = fs.readFileSync('src/json/accounts.json', 'utf8')
-const accounts = JSON.parse(accountData)
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
+const accounts = JSON.parse(accountData);
 
-const userData = fs.readFileSync('src/json/users.json', 'utf8')
-const users = JSON.parse(userData)
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
+const users = JSON.parse(userData);
 
-app.get('/', (req, res) =>{
-    res.render('index', {title: 'Account Summary', accounts})
-})
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
 
-app.get('/savings', (req, res)=>{
-    res.render('account',{account:accounts.savings})
-})
-app.get('/checking', (req, res)=>{
-    res.render('account',{account:accounts.savings})
-})
-app.get('/credit', (req, res)=>{
-    res.render('account',{account:accounts.savings})
-})
+app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
+app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
+app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
 
-app.get('/profile',(req, res)=>{
-    res.render('profile', {user:users[0]})
-})
+app.get('/transfer', (req, res) =>  res.render('transfer'));
 
-app.get('/transfer', (req, res)=>{
-    res.render('transfer')
-})
+app.post('/transfer', (req, res) => {
+    accounts[req.body.from].balance -= req.body.amount;
+    accounts[req.body.to].balance += parseInt(req.body.amount, 10);
+    let accountsJSON = JSON.stringify(accounts, null, 4)
+    fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+    res.render('transfer', {message: 'Transfer Completed'});
+});
 
-app.post('/transfer',(req, res)=>{ 
-    const {from, to, amount} = req.body
+app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit}));
+app.post('/payment', (req, res) => {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount);
+    let accountsJSON = JSON.stringify(accounts, null, 4)
+    fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+    res.render('payment', {message: 'Payment Successful', account: accounts.credit});
+});
 
-    let fromAccount = accounts[from].balance
-    let toAccount = accounts[to].balance
-    fromAccount -= parseFloat(amount)
-    toAccount += parseFloat(amount)
+app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
 
-    accounts[from].balance = fromAccount
-    accounts[to].balance = toAccount
-
-    const accountsJSON = JSON.stringify(accounts)
-
-    fs.writeFileSync('./json/accounts.json',accountsJSON, 'utf8')
-
-    res.render('transfer', {message: "Transfer Completed"})
-
-})
-
-app.get('/payment',(req, res)=>{
-
-    res.render('payment', {account: accounts.credit})
-})
-
-app.post('/payment',(req, res)=>{
-    accounts.credit.balance = accounts.credit.balance - parseInt(req.body.amount)
-    accounts.credit.available = parseInt(req.body.amount) + accounts.credit.available
-
-    const accountsJSON = JSON.stringify(accounts)
-    fs.writeFileSync('./json/accounts.json', accountsJSON, 'utf8')
-
-    res.render('payment',{ message: "Payment Successful", account: accounts.credit })
-})
-
-
-//create a function to add 2 numbers
-const addTwoNumbers = function(a,b){
-    return a+b
-}
-
-// a function to stringify json data
-const stringify = (json)=>{
-       return JSON.stringify(json)
-}
-
-
-
-app.listen(3000,()=>{
-    console.log('PS Project Running on port 3000!')
-})
+app.listen(3000, () => { console.log('PS Project Running on port 3000!') });
